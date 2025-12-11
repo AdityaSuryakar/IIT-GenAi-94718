3cb397694310af804682a24807ad12da054a8437
Aditya Suryakar <suryakaraditya@gmail.com>
2025-12-11 09:35:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import arith as arithmetic
+a = 10
+b = 5
+print("Addition:", arithmetic.add(a, b))
+print("Subtraction:", arithmetic.subtract(a, b))
+print("Multiplication:", arithmetic.multiply(a, b))
+print("Division:", arithmetic.divide(a, b))
